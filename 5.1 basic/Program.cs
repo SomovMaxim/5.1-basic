b664143e084af769f9173fded5a58b657a696517
Максим Сомов <catfishvip@mail.ru>
2020-12-23 22:15:06 +0200

    Добавьте файлы проекта.

---
+++ 5.1 basic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = 99;
+            for (int i = 10; i <= 99; i++)
+            {
+                bool b = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0 & i % 1 == 0)
+                    {
+                        b = false;
+                    }
+                }
+                if (b)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
